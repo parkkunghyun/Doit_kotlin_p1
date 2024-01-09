75c97b89edc8fb83475cf40587d06d15b3cadc88
baggyeonghyeon <rudgus4620@gmail.com>
2024-01-09 15:58:36 +0900

    7강

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Doit_kotlin"
+include(":app")
+include(":ch7_layout")
